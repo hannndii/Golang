36f3a70aadd971c233f6830e9c00ae489443e82d
hannndii <hanndi287@gmail.com>
2024-02-27 08:34:50 +0700

    fac-per.go

---
+++ fac-per.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+)
+
+// Fungsi untuk menghitung nilai faktorial
+func factorial(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+
+// Fungsi untuk menghitung nilai permutasi
+func permutation(x, y int) int {
+    if x >= y {
+        return factorial(x) / factorial(x-y)
+    }
+    return factorial(y) / factorial(y-x)
+}
+
+func main() {
+    var x, y int
+
+    // Meminta masukan dari pengguna
+    fmt.Println("Masukkan dua bilangan bulat:")
+    fmt.Scan(&x, &y)
+
+    // Menghitung nilai faktorial x dan y
+    factX := factorial(x)
+    factY := factorial(y)
+
+    // Menghitung nilai permutasi x dan y
+    perm := permutation(x, y)
+
+    // Menampilkan hasilnya
+    fmt.Printf("%d %d %d\n", factX, factY, perm)
+}
